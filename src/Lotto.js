ef58e4b3685e5a9d9e7b08ad55e5d99991d59649
Yooaknow <dbdkfma1228@naver.com>
2025-10-31 03:01:33 +0900

    feat(lotto): 번호가 1~45 범위를 벗어날 경우 예외 발생 로직 구현
    로또 번호가 1~45만 되도록 로직을 구현했다.

--- src/Lotto.js
+++ src/Lotto.js
@@ -10,7 +10,15 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+    
+  numbers.forEach((num)=> {
+    if (!Number.isInteger(num) || num <1 || num > 45) {
+      throw new Error ("[ERROR] 로또 번호는 1부터 45 사이의 숫자입니다.")
+    }
+  });
   }
+
+
 
   // TODO: 추가 기능 구현
 }
