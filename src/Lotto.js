3a0f706c016e0f1f06b8009e59ddab2189c90df1
Yooaknow <dbdkfma1228@naver.com>
2025-11-02 01:08:05 +0900

    test(generator): LottoGenerator 동작 테스트 추가
    pick 호출과 Lotto 인스턴스 반환을 검증했습니다

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,9 +1,11 @@
+import { Construction } from "lucide-react";
+
 class Lotto {
   #numbers;
 
   constructor(numbers) {
     this.#validate(numbers);
-    this.#numbers = numbers;
+    this.#numbers = [...numbers].sort((a,b)=> a-b);
   }
 
   #validate(numbers) {
@@ -13,14 +15,18 @@ class Lotto {
 
   const set = new Set(numbers);
   if (set.size !== 6){
-    throw new Error("[ERROR] 로또 번호는 중복될 수 없습니다");
+    throw new Error("[ERROR] 로또 번호는 중복될 수 없습니다.");
   }
+
+
     
   numbers.forEach((num)=> {
     if (!Number.isInteger(num) || num <1 || num > 45) {
       throw new Error ("[ERROR] 로또 번호는 1부터 45 사이의 숫자입니다.")
     }
   });
+
+  
   }
 
 
