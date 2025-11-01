550c1ecf9736d76b020a2099094a922721e18aca
Yooaknow <dbdkfma1228@naver.com>
2025-11-02 01:05:56 +0900

    test(generator): LottoGenerator 동작 테스트 추가

---
+++ __tests__/LottoGeneratorTest.js
@@ -0,0 +1,16 @@
+import Lotto from "../src/Lotto";
+import LottoGenerator from "../src/LottoGenerator";
+
+describe("LottoGenerator", () => {
+  test("의존성 주입된 랜덤 함수로 로또를 생성한다.", () => {
+    const fakePick = () => [6, 1, 45, 10, 2, 3];
+    const lotto = LottoGenerator.generate(fakePick);
+    expect(lotto).toBeInstanceOf(Lotto);
+  });
+
+  test("의존성 주입된 pick이 올바른 파라미터(1, 45, 6)로 호출된다.", () => {
+    const fakePick = jest.fn().mockReturnValue([1, 2, 3, 4, 5, 6]);
+    LottoGenerator.generate(fakePick);
+    expect(fakePick).toHaveBeenCalledWith(1, 45, 6);
+  });
+});
