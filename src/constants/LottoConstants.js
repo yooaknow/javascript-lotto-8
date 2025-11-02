5cffd488e639a81c701600c26ec01c387a1a29b0
Yooaknow <dbdkfma1228@naver.com>
2025-11-03 01:52:00 +0900

    feat(constants): 당첨 상수 및 매치 개수 상수 추가
    PRIZE와 MATCH_COUNT를 상수로 정의하여 하드코딩을 제거하도록 했습니다.

--- src/constants/LottoConstants.js
+++ src/constants/LottoConstants.js
@@ -15,3 +15,19 @@ export const ERROR_MESSAGES = {
   DUPLICATE_BONUS_NUMBER: "[ERROR] 보너스 번호가 당첨 번호와 중복될 수 없습니다.",
   INVALID_WINNING_NUMBERS: "[ERROR] 당첨 번호는 6개여야 합니다.",
 };
+
+export const PRIZE = {
+  FIRST: 2000000000,
+  SECOND: 30000000,
+  THIRD: 1500000,
+  FOURTH: 50000,
+  FIFTH: 5000,
+};
+
+export const MATCH_COUNT = {
+  FIRST: 6,
+  SECOND: 5,
+  THIRD: 5,
+  FOURTH: 4,
+  FIFTH: 3,
+};
