b6439dd9b8f383d5960979a216d3710a2eae2b21
Yooaknow <dbdkfma1228@naver.com>
2025-11-03 01:37:52 +0900

    feat(constants): 로또 관련 상수 파일 생성
     하드코딩 제거를 위해 상수 파일을 생성했습니다.

---
+++ src/constants/LottoConstants.js
@@ -0,0 +1,17 @@
+export const LOTTO_PRICE = 1000;
+
+export const NUMBER_RANGE = {
+  MIN: 1,
+  MAX: 45,
+};
+
+export const LOTTO_NUMBERS_COUNT = 6;
+
+export const ERROR_MESSAGES = {
+  INVALID_PURCHASE_AMOUNT: "[ERROR] 구입 금액은 1000원 단위여야 합니다.",
+  INVALID_LOTTO_COUNT: "[ERROR] 로또 번호는 6개여야 합니다.",
+  DUPLICATE_LOTTO_NUMBER: "[ERROR] 로또 번호는 중복될 수 없습니다.",
+  INVALID_LOTTO_NUMBER: "[ERROR] 로또 번호는 1부터 45 사이의 숫자입니다.",
+  DUPLICATE_BONUS_NUMBER: "[ERROR] 보너스 번호가 당첨 번호와 중복될 수 없습니다.",
+  INVALID_WINNING_NUMBERS: "[ERROR] 당첨 번호는 6개여야 합니다.",
+};
