ec18ae2c07bffdae947f681442d7c3cce961b272
Yooaknow <dbdkfma1228@naver.com>
2025-11-03 01:29:34 +0900

    refactor(Validator): 입력 검증 상수화 적용
    구입 금액, 당첨 번호, 보너스 번호 검증 로직을 업데이트했습니다.

--- src/util/Validator.js
+++ src/util/Validator.js
@@ -1,19 +1,33 @@
-export default class Validator {
-  static validatePurchaseAmount(amount) {
-    if (amount % 1000 !== 0) {
-      throw new Error("[ERROR] 구입 금액은 1000원 단위여야 합니다.");
-    }
+import { LOTTO_NUMBERS_COUNT, NUMBER_RANGE, ERROR_MESSAGES } from "../constants/LottoConstants.js";
+
+class Lotto {
+  #numbers;
+
+  constructor(numbers) {
+    this.#validate(numbers);
+    this.#numbers = [...numbers].sort((a, b) => a - b);
   }
 
-  static validateWinningNumbers(numbers) {
-    if (!Array.isArray(numbers) || numbers.length !== 6) {
-      throw new Error("[ERROR] 당첨 번호는 6개여야 합니다.");
-    }
+  get numbers() {
+    return this.#numbers;
   }
 
-  static validateBonusNumber(bonus, winningNumbers) {
-    if (winningNumbers.includes(bonus)) {
-      throw new Error("[ERROR] 보너스 번호가 당첨 번호와 중복될 수 없습니다.");
+  #validate(numbers) {
+    if (numbers.length !== LOTTO_NUMBERS_COUNT) {
+      throw new Error(ERROR_MESSAGES.INVALID_LOTTO_COUNT);
     }
+
+    const set = new Set(numbers);
+    if (set.size !== LOTTO_NUMBERS_COUNT) {
+      throw new Error(ERROR_MESSAGES.DUPLICATE_LOTTO_NUMBER);
+    }
+
+    numbers.forEach((num) => {
+      if (!Number.isInteger(num) || num < NUMBER_RANGE.MIN || num > NUMBER_RANGE.MAX) {
+        throw new Error(ERROR_MESSAGES.INVALID_LOTTO_NUMBER);
+      }
+    });
   }
 }
+
+export default Lotto;
