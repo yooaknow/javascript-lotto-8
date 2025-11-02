de672a54209630cdb0c696b28657fb1cab935e34
Yooaknow <dbdkfma1228@naver.com>
2025-11-03 02:30:50 +0900

    refactor(Validator): Validator 클래스  상수 사용으로 리팩토링
    Validator 클래스의 하드코딩된 값들을 상수로 변경했습니다.

--- src/util/Validator.js
+++ src/util/Validator.js
@@ -1,33 +1,41 @@
-import { LOTTO_NUMBERS_COUNT, NUMBER_RANGE, ERROR_MESSAGES } from "../constants/LottoConstants.js";
+import { LOTTO_NUMBERS_COUNT, NUMBER_RANGE, LOTTO_PRICE, ERROR_MESSAGES } from "../constants/LottoConstants.js";
 
-class Lotto {
-  #numbers;
-
-  constructor(numbers) {
-    this.#validate(numbers);
-    this.#numbers = [...numbers].sort((a, b) => a - b);
+export default class Validator {
+  static validatePurchaseAmount(amount) {
+    if (amount % LOTTO_PRICE !== 0) {
+      throw new Error(ERROR_MESSAGES.INVALID_PURCHASE_AMOUNT);
+    }
   }
 
-  get numbers() {
-    return this.#numbers;
+  static validateWinningNumbers(numbers) {
+    if (!Array.isArray(numbers) || numbers.length !== LOTTO_NUMBERS_COUNT) {
+      throw new Error(ERROR_MESSAGES.INVALID_WINNING_NUMBERS);
+    }
+    this.#validateNumberRange(numbers);
+    this.#validateDuplicate(numbers);
   }
 
-  #validate(numbers) {
-    if (numbers.length !== LOTTO_NUMBERS_COUNT) {
-      throw new Error(ERROR_MESSAGES.INVALID_LOTTO_COUNT);
-    }
-
-    const set = new Set(numbers);
-    if (set.size !== LOTTO_NUMBERS_COUNT) {
-      throw new Error(ERROR_MESSAGES.DUPLICATE_LOTTO_NUMBER);
-    }
-
+  static #validateNumberRange(numbers) {
     numbers.forEach((num) => {
       if (!Number.isInteger(num) || num < NUMBER_RANGE.MIN || num > NUMBER_RANGE.MAX) {
         throw new Error(ERROR_MESSAGES.INVALID_LOTTO_NUMBER);
       }
     });
   }
-}
 
-export default Lotto;
+  static #validateDuplicate(numbers) {
+    const set = new Set(numbers);
+    if (set.size !== LOTTO_NUMBERS_COUNT) {
+      throw new Error(ERROR_MESSAGES.DUPLICATE_LOTTO_NUMBER);
+    }
+  }
+
+  static validateBonusNumber(bonus, winningNumbers) {
+    if (!Number.isInteger(bonus) || bonus < NUMBER_RANGE.MIN || bonus > NUMBER_RANGE.MAX) {
+      throw new Error(ERROR_MESSAGES.INVALID_LOTTO_NUMBER);
+    }
+    if (winningNumbers.includes(bonus)) {
+      throw new Error(ERROR_MESSAGES.DUPLICATE_BONUS_NUMBER);
+    }
+  }
+}
