5efb6a118107adaa48a78a0365308522aaa06971
Yooaknow <dbdkfma1228@naver.com>
2025-11-03 00:41:11 +0900

    feat(App): 전체 게임 연결
    InputView, OutputView, GameService 연결 및 실행 진입점을 추가했습니다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
-class App {
-  async run() {}
+import InputView from "./view/InputView.js";
+import OutputView from "./view/OutputView.js";
+import GameService from "./service/GameService.js";
+
+export default class App {
+  constructor() {
+    this.service = new GameService();
+  }
+
+  async run() {
+    const purchaseAmount = await InputView.readPurchaseAmount();
+
+    const lottos = this.service.generateLottos(purchaseAmount);
+    OutputView.printLottos(lottos);
+
+    const winningNumbers = await InputView.readWinningNumbers();
+    const bonusNumber = await InputView.readBonusNumber(winningNumbers);
+
+    const statistics = this.service.tallyResults(lottos, winningNumbers, bonusNumber);
+
+    OutputView.printStatistics(statistics, purchaseAmount);
+  }
 }
 
-export default App;
+if (process.argv[1].includes("App.js")) {
+  const app = new App();
+  app.run();
+}
